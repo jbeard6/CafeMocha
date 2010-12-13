7cc3767638be522d562ed1b09fb5279f7606ef81
Joseph Beard <joseph@josephbeard.net>
2010-12-13 02:48:59 +0000

    Set keywords on all existing Java files.

--- swing-application/src/main/java/net/sf/cafemocha/application/swing/SwingApplication.java
+++ swing-application/src/main/java/net/sf/cafemocha/application/swing/SwingApplication.java
@@ -16,6 +16,17 @@ import net.sf.cafemocha.application.Application;
  */
 public class SwingApplication extends Application {
 
+	/*
+	 * Convenience method that returns the covariant SwingApplicationContext.
+	 * 
+	 * @see net.sf.cafemocha.application.Application#getContext()
+	 */
+	@Override
+	public SwingApplicationContext getContext() {
+		// TODO Enforce safety of cast through constructor
+		return (SwingApplicationContext) super.getContext();
+	}
+
 	// TODO Add Look and Feel, Window location preferences, etc.
 
 }
